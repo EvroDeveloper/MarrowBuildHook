82b1e2705a9cafb14100f2db813792e2ecd5e9bf
holadivinus <holadivinus@gmail.com>
2025-07-25 15:35:18 -0700

    Better Error avoidance and post-process restoration

--- MarrowBuildHook.cs
+++ MarrowBuildHook.cs
@@ -12,6 +12,7 @@ using UnityEditor.AddressableAssets.Build.DataBuilders;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.Assertions.Must;
+using static UnityEngine.EventSystems.EventTrigger;
 
 namespace MarrowBuildHook
 {
@@ -66,33 +67,66 @@ namespace MarrowBuildHook
             }
 
             List<string> errors = new();
-            Dictionary<string, string>  modifiedPrefabs = new();
+            Dictionary<string, string> modifiedPrefabs = new();
+
+            UnityEditor.EditorApplication.CallbackFunction fixup = null;
+            fixup = () =>
+            {
+                var dictCopy = modifiedPrefabs.ToArray();
+                foreach (var changed in dictCopy)
+                {
+                    try
+                    {
+                        File.WriteAllText(changed.Key, changed.Value);
+                        modifiedPrefabs.Remove(changed.Key);
+                        Debug.LogWarning("[MBH] Restored prefab: " + changed.Key);
+                    } catch(Exception)
+                    {
+                        Debug.LogWarning("[MBH] Failed to restore prefab: " + changed.Key);
+                        Debug.LogWarning("[MBH] Retrying...");
+                        // in case of restoration failure, we'll try again next delayCall
+                    }
+                }
+                if (modifiedPrefabs.Count > 0)
+                    EditorApplication.delayCall += fixup; // (try again ...)
+                else
+                {
+                    // Finally, force reimport the restored prefabs.
+                    AssetDatabase.Refresh();
+                }
+            };
+            EditorApplication.delayCall += fixup;
+
             foreach (var entry in aaContext.assetEntries)
             {
                 if (entry.MainAssetType == typeof(GameObject))
                 {
-                    if (entry.AssetPath.Contains(".obj") || entry.AssetPath.Contains(".fbx") || entry.AssetPath.Contains(".blend")) continue;
+                    if (entry.AssetPath.EndsWith(".obj") || entry.AssetPath.EndsWith(".fbx") || entry.AssetPath.EndsWith(".blend")) continue;
                     modifiedPrefabs.Add(entry.AssetPath, File.ReadAllText(entry.AssetPath));
 
-                    var prefab = PrefabUtility.LoadPrefabContents(entry.AssetPath);
                     try
                     {
-                        ProcessGameObject(new GameObject[] { prefab }); // yayyyyy, array allocation
-                        PrefabUtility.SaveAsPrefabAsset(prefab, entry.AssetPath);
-                    } catch(Exception ex) 
+                        var prefab = PrefabUtility.LoadPrefabContents(entry.AssetPath);
+                        try
+                        {
+                            ProcessGameObject(new GameObject[] { prefab }); // yayyyyy, array allocation
+                            PrefabUtility.SaveAsPrefabAsset(prefab, entry.AssetPath);
+                        }
+                        catch (Exception ex)
+                        {
+                            Debug.LogWarning("[MBH]: Failed to Process prefab: " + entry.AssetPath);
+                            Debug.LogWarning(ex);
+                        }
+                        PrefabUtility.UnloadPrefabContents(prefab);
+                    } catch (Exception ex)
                     {
                         Debug.LogWarning("[MBH]: Failed to Process prefab: " + entry.AssetPath);
                         Debug.LogWarning(ex);
                     }
-                    PrefabUtility.UnloadPrefabContents(prefab);
                 }
             }
 
-            EditorApplication.delayCall += () =>
-            {
-                foreach (var changed in modifiedPrefabs)
-                    File.WriteAllText(changed.Key, changed.Value);
-            };
+            
         }
 
         public static void BigPrint(object msg)
@@ -108,13 +142,33 @@ namespace MarrowBuildHook
                 try
                 {
                     externalPass.Invoke(gameObject);
-                } catch(Exception) { }
+                } catch(Exception ex) 
+                {
+                    Debug.LogWarning("[MBH]: Failed to Process externalPass for gameobjects: [" + string.Join(", ", gameObject.Select(g => g.name)) + "]");
+                    Debug.LogWarning(ex);
+                }
             }
 
-            var passes = gameObject.SelectMany(g => g.GetComponentsInChildren<IBuildPass>(true)).Where(bp => bp.PassWhenInactive || ((Component)bp).gameObject.activeInHierarchy).ToList();
-            passes.Sort((a, b) => Comparer<int>.Default.Compare(a.PassPriority, b.PassPriority));
-            foreach (var pass in passes)
-                pass.OnBuild();
+            try
+            {
+                var passes = gameObject.SelectMany(g => g.GetComponentsInChildren<IBuildPass>(true)).Where(bp => bp.PassWhenInactive || ((Component)bp).gameObject.activeInHierarchy).ToList();
+                passes.Sort((a, b) => Comparer<int>.Default.Compare(a.PassPriority, b.PassPriority));
+                foreach (var pass in passes)
+                {
+                    try
+                    {
+                        pass.OnBuild();
+                    } catch(Exception ex)
+                    {
+                        Debug.LogWarning("[MBH]: Failed to Execute IBuildPass " + pass.GetType().Name + " on " + ((Component)pass).gameObject.name);
+                        Debug.LogWarning(ex);
+                    }
+                }
+            } catch(Exception ex)
+            {
+                Debug.LogWarning("[MBH]: Failed to Collect IBuildPasses for gameobjects: [" + string.Join(", ", gameObject.Select(g => g.name)) + "]");
+                Debug.LogWarning(ex);
+            }
         }
     }
 }
